e10ef4a75366d5a7c9c2e1fa6b682cceb47138ca
jonatastonicioli <tonicioli28@gmail.com>
2026-03-25 10:36:09 -0300

    ex. da distancia

--- CMPLGP1/24.03.2026/calculo.c
+++ CMPLGP1/24.03.2026/calculo.c
@@ -22,19 +22,42 @@ void exerc1()
  int randomico;
 
     randomico  = 1 + rand( ) % ( 6 - 1 + 1 );
-    printf("O valor sorteado de 1~6 e %d", randomico);
+    printf("\nO valor sorteado de 1~6 e %d", randomico);
 
 }
 void exerc3()
 {
+int xa, ya, xb, yb;
+double subx, suby, potenciax, potenciay, raiz;
+printf("Digite o valor xa: ");
+scanf("%i", &xa);
+printf("\nDigite o valor ya: ");
+scanf("%i", &ya);
+printf("\nDigite o valor xb: ");
+scanf("%i", &xb);
+printf("\nDigite o valor yb: ");
+scanf("%i", &yb);
+
+subx = (xb - xa);
+suby = (yb - ya);
+
+potenciax = pow(subx, 2);
+potenciay = pow(suby, 2);
+
+raiz = sqrt(potenciax + potenciay);
+
+printf("o valor de subx e: %lf", subx);
+printf("\nO valor da distancia e: %lf", raiz);
+
 
 }
 
+
 int main()
 {
-    exerc1();
-    exerc2();
     exerc3();
+    //exerc1();
+    //exerc2();
     return 0;
 
 }
