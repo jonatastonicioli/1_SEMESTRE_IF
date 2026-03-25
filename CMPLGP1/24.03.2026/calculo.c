0d19f343d605741495b78b745dccbb83fd388d5d
jonatastonicioli <tonicioli28@gmail.com>
2026-03-25 10:57:53 -0300

    att

--- CMPLGP1/24.03.2026/calculo.c
+++ CMPLGP1/24.03.2026/calculo.c
@@ -5,6 +5,9 @@
 
 void exerc2()
 {
+/*Crie um programa em C que leia dois valores reais (y e z) e que calcule o valor de x, considerando que:
+ x = y² + z³ + 1. */
+
   float x,y,z, potenciay,potenciaz,calculo;
     printf("\nEscreva o valor de y: ");
     scanf("%f", &y);
@@ -19,6 +22,7 @@ void exerc2()
 
 void exerc1()
 {
+/* Crie um programa em C que simule o lançamento de um dado de 6 faces de forma randomica*/
  int randomico;
 
     randomico  = 1 + rand( ) % ( 6 - 1 + 1 );
@@ -26,6 +30,8 @@ void exerc1()
 
 }
 void exerc3()
+/*Crie um programa que receba 4 valores representando as coordenadas de dois pontos A e B: A(xa,ya) e B(xb, yb). Em seguida,
+calcule e mostre na tela a distância entre eles. */
 {
 int xa, ya, xb, yb;
 double subx, suby, potenciax, potenciay, raiz;
@@ -46,8 +52,29 @@ potenciay = pow(suby, 2);
 
 raiz = sqrt(potenciax + potenciay);
 
-printf("o valor de subx e: %lf", subx);
 printf("\nO valor da distancia e: %lf", raiz);
+
+
+}
+void exerc4()
+{
+#define PI 3.1415
+double ang, altura, escada, radianos, seno;
+
+printf("\nDigite o angulo entre a escada e o chao: ");
+scanf("%lf", &ang);
+printf("\nDigite a altura da parede onde a escada esta apoiada: ");
+scanf("%lf", &altura);
+
+// tranformando graus para radianos
+radianos = PI * (ang/180);
+
+// calculando o seno
+seno = sin(radianos);
+
+escada = (altura/seno);
+printf("\nO valor da escada e: %lf", escada);
+
 
 
 }
@@ -55,7 +82,8 @@ printf("\nO valor da distancia e: %lf", raiz);
 
 int main()
 {
-    exerc3();
+    exerc4();
+    //exerc3();
     //exerc1();
     //exerc2();
     return 0;
